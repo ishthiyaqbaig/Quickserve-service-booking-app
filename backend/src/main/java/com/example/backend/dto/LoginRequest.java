3ea425e96a23337d14d13c1f56a9bd451f75f93a
M Ishthiyaq Baig <ishthiyaqbaig@gmail.com>
2025-12-11 21:13:22 +0530

    Delete backend directory

--- backend/src/main/java/com/example/backend/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    public String email;
-    public String password;
-}
--- backend/src/main/java/com/example/backend/enums/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.enums;
-
-public enum Role {
-    ADMIN,
-    CUSTOMER,
-    SERVICE_PROVIDER
-}
-
